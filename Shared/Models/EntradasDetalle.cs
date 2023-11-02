9da653c1d6ec9fb502bc6c7f129a5b86e7d628a5
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2023-11-02 19:26:36 -0400

    Agreando Validaciones al Modelo EntradasDetalle

--- Shared/Models/EntradasDetalle.cs
+++ Shared/Models/EntradasDetalle.cs
@@ -1,13 +1,17 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 public class EntradasDetalle
 {
     [Key]
     public int DetalleId {get; set;}
 
+    [ForeignKey("EntradaId")]
     public int EntradaId {get; set;}
 
+    [ForeignKey("ProductoId")]
     public int ProductoId {get; set;}
-
+    [Required(ErrorMessage ="El campo {0} es obligatorio")]
+    [Range(1,200, ErrorMessage ="EL dato insertado en el campo {0} debe de estar entre {1} y {2}")]
     public float CantidadUtilizada {get; set;}
 }
