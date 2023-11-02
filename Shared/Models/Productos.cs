1d51af7f6d4c72c5ac22be481d4f7a8cce63c90f
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2023-11-02 19:35:04 -0400

    Agregando validaciones al Modelo Productos

--- Shared/Models/Productos.cs
+++ Shared/Models/Productos.cs
@@ -1,13 +1,23 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 public class Productos
 {
     [Key]
     public int ProductoId {get; set;}
 
+    [Required(ErrorMessage ="El campo {0} es obligatorio")]
+    [StringLength(50, MinimumLength = 3, ErrorMessage ="Debe de insertar un dato entre {2} y {1} caracteres")]
     public string Descripcion {get; set;} = string.Empty;
 
+    [Required(ErrorMessage ="El campo {0} es obligatorio")]
+    [Range(0,15, ErrorMessage ="El dato insertado debe de estar entre {1} y {2}")]
     public short Tipo {get; set;}
-
+    
+    [Required(ErrorMessage ="El campo {0} es obligatorio")]
+    [Range(0,1500, ErrorMessage ="El dato insertado debe de estar entre {1} y {2}")]
     public int Existencia {get; set;}
+
+    [ForeignKey("Productoid")]
+    public ICollection<EntradasDetalle> EntradasDetalles {get; set;} = new List<EntradasDetalle>();
 }
