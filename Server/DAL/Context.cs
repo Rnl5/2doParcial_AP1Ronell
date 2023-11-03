9a360a0cf3901a8b3af1777b215ac2b46b05f3a7
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2023-11-02 21:16:34 -0400

    Agregando en el contexto el modelo Productos

--- Server/DAL/Context.cs
+++ Server/DAL/Context.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore;
 public class Context : DbContext
 {
    public Context(DbContextOptions<Context> options) : base(options) { }
-
+   
    public DbSet<Entradas> Entradas {get; set;}
+
+   public DbSet<Productos> Productos {get; set;}
 }
