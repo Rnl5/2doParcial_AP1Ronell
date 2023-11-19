abd1c090ae4f55f1fece515307a8b91946aa0b1e
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2023-11-18 23:41:45 -0400

    Agregando base de datos y migraciones

---
+++ Server/Migrations/20231112032015_Inicial.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace _2doParcial_AP1RonellIntento.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Entradas",
+                columns: table => new
+                {
+                    EntradaId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Concepto = table.Column<string>(type: "TEXT", maxLength: 75, nullable: false),
+                    PesoTotal = table.Column<int>(type: "INTEGER", nullable: false),
+                    ProductoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CantidadProducida = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Entradas", x => x.EntradaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    ProductoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Tipo = table.Column<short>(type: "INTEGER", nullable: false),
+                    Existencia = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.ProductoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EntradasDetalle",
+                columns: table => new
+                {
+                    DetalleId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    EntradaId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ProductoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CantidadUtilizada = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EntradasDetalle", x => x.DetalleId);
+                    table.ForeignKey(
+                        name: "FK_EntradasDetalle_Entradas_EntradaId",
+                        column: x => x.EntradaId,
+                        principalTable: "Entradas",
+                        principalColumn: "EntradaId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EntradasDetalle_Productos_ProductoId",
+                        column: x => x.ProductoId,
+                        principalTable: "Productos",
+                        principalColumn: "ProductoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Productos",
+                columns: new[] { "ProductoId", "Descripcion", "Existencia", "Tipo" },
+                values: new object[,]
+                {
+                    { 1, "Mani", 50, (short)0 },
+                    { 2, "Pistachos", 600, (short)0 },
+                    { 3, "Pasas", 500, (short)0 },
+                    { 4, "Ciruelas", 700, (short)0 },
+                    { 5, "Mixto MPP 0.5 lb", 0, (short)1 },
+                    { 6, "Mixto MPC 0.5 lb", 0, (short)1 },
+                    { 7, "Mixto MPP 0.2 lb", 0, (short)1 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EntradasDetalle_EntradaId",
+                table: "EntradasDetalle",
+                column: "EntradaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EntradasDetalle_ProductoId",
+                table: "EntradasDetalle",
+                column: "ProductoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EntradasDetalle");
+
+            migrationBuilder.DropTable(
+                name: "Entradas");
+
+            migrationBuilder.DropTable(
+                name: "Productos");
+        }
+    }
+}
---
+++ Server/Migrations/20231118211629_Agregando EntradasDetalle al context.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _2doParcial_AP1RonellIntento.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregandoEntradasDetallealcontext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_EntradasDetalle_Entradas_EntradaId",
+                table: "EntradasDetalle");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_EntradasDetalle_Productos_ProductoId",
+                table: "EntradasDetalle");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_EntradasDetalle",
+                table: "EntradasDetalle");
+
+            migrationBuilder.RenameTable(
+                name: "EntradasDetalle",
+                newName: "EntradasDetalles");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_EntradasDetalle_ProductoId",
+                table: "EntradasDetalles",
+                newName: "IX_EntradasDetalles_ProductoId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_EntradasDetalle_EntradaId",
+                table: "EntradasDetalles",
+                newName: "IX_EntradasDetalles_EntradaId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_EntradasDetalles",
+                table: "EntradasDetalles",
+                column: "DetalleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EntradasDetalles_Entradas_EntradaId",
+                table: "EntradasDetalles",
+                column: "EntradaId",
+                principalTable: "Entradas",
+                principalColumn: "EntradaId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EntradasDetalles_Productos_ProductoId",
+                table: "EntradasDetalles",
+                column: "ProductoId",
+                principalTable: "Productos",
+                principalColumn: "ProductoId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_EntradasDetalles_Entradas_EntradaId",
+                table: "EntradasDetalles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_EntradasDetalles_Productos_ProductoId",
+                table: "EntradasDetalles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_EntradasDetalles",
+                table: "EntradasDetalles");
+
+            migrationBuilder.RenameTable(
+                name: "EntradasDetalles",
+                newName: "EntradasDetalle");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_EntradasDetalles_ProductoId",
+                table: "EntradasDetalle",
+                newName: "IX_EntradasDetalle_ProductoId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_EntradasDetalles_EntradaId",
+                table: "EntradasDetalle",
+                newName: "IX_EntradasDetalle_EntradaId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_EntradasDetalle",
+                table: "EntradasDetalle",
+                column: "DetalleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EntradasDetalle_Entradas_EntradaId",
+                table: "EntradasDetalle",
+                column: "EntradaId",
+                principalTable: "Entradas",
+                principalColumn: "EntradaId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EntradasDetalle_Productos_ProductoId",
+                table: "EntradasDetalle",
+                column: "ProductoId",
+                principalTable: "Productos",
+                principalColumn: "ProductoId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
