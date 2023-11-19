8c4b929705ea9c9e12676aa1bf8d078cf05f2678
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2023-11-18 22:23:55 -0400

    Agregando el NotificactionService

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,10 +1,13 @@
 using _2doParcial_AP1RonellIntento.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped<NotificationService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
